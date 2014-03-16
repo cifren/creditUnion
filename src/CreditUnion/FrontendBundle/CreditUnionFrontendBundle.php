be7a5c9d8f4b7fc895b4067672b405018cbe450e
cifren <cifren@msn.com>
2014-03-15 22:37:39 -0500

    replace datetime2 by datetime

--- src/CreditUnion/FrontendBundle/CreditUnionFrontendBundle.php
+++ src/CreditUnion/FrontendBundle/CreditUnionFrontendBundle.php
@@ -6,8 +6,5 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class CreditUnionFrontendBundle extends Bundle
 {
-    public function boot()
-    {
-        \Doctrine\DBAL\Types\Type::overrideType('datetime', 'Doctrine\DBAL\Types\DatetimeType');
-    }
+    
 }
